68a15aaebd86d5f8101ab770bdca1b88a8dfd025
yn0315 <yunu0315@naver.com>
2023-03-05 16:20:03 +0900

    [2023.03.05]

--- app.js
+++ app.js
@@ -173,8 +173,8 @@ app.post('/loginAction', function (req, res) {
             if (results[0].user_pw === $pw) {
                 console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
                 res.header('Content-Type','text/plain');
-                res.send('200')
-                res.render(__dirname+ '/views/index.ejs', {users : results}); 
+                res.send('200',res.render(__dirname+ '/views/index.ejs', {users : results}))
+                
                 // getlist.ejs 에 render 해줄건데 , users 에 쿼리문 날리고난 results 를 담을거다 
             }
         }
