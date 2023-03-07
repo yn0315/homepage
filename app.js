d6cbad2659c61f600f0f85049dd17f0e1bd0e917
yn0315 <yunu0315@naver.com>
2023-03-07 21:56:50 +0900

    [2023.03.07]

--- app.js
+++ app.js
@@ -163,11 +163,11 @@ app.post('/loginAction', function (req, res) {
             console.log("1111111111111111111111");
             if (results[0].user_pw === $pw) {
                 console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-                // res.header('Content-Type','text/plain');
+                res.header('Content-Type','text/plain');
                 // res.send('200', {user: results});
-                // res.send('200');
+                res.send('200');
                 console.log(results);
-                res.render(__dirname + '/views/index.ejs', { users: results });
+               
 
 
 
@@ -178,6 +178,8 @@ app.post('/loginAction', function (req, res) {
 
 
     });
+
+    res.render(__dirname + '/views/index.ejs', { users: results });
 
 
 });
