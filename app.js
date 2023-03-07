98d2e32efc0cb78fee23c68cde35a06b57d66284
yn0315 <yunu0315@naver.com>
2023-03-08 02:55:52 +0900

    [2023.03.07]

--- app.js
+++ app.js
@@ -169,7 +169,8 @@ app.post('/loginAction', function (req, res) {
                 console.log(results);
                 // res.redirect('/', {user: results});
                 // res.send('200');
-                res.render(__dirname + '/views/index.ejs', { users: results });
+                // res.render(__dirname + '/views/index.ejs', { users: results });
+                res.render(__dirname + '/views/index.ejs');
 
 
 
