5ba51f6c9013de5bddda0e16c8933f5d548b4d56
yn0315 <yunu0315@naver.com>
2023-03-12 14:10:48 +0900

    [2023.03.12]

--- app.js
+++ app.js
@@ -204,7 +204,7 @@ app.post('/loginAction', function (req, res) {
       if (results[0] && results[0].user_id === $id && results[0].user_pw === $pw) {
         // res.redirect(`/user/${results[0].user_id}`); // 로그인이 성공했을 때 로그인 정보를 함께 전달
         res.header('Content-Type', 'text/plain');
-        res.send('200');
+        res.send('200',res.render(__dirname + '/views/index.ejs', {data : results[0].user_id}));
       }else {
           res.send('500');
       }
